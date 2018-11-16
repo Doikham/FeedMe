422d1a52d83e076ba252e56f00199ea7aea0c201
DESKTOP-I58HO27\Neramit Singh <neramit.singh@gmail.com>
2018-11-16 19:02:43 +0700

    Added LoginActivity and Signup Activity Functionality

--- app/src/main/java/com/egci428/feedme/LoginActivity.kt
+++ app/src/main/java/com/egci428/feedme/LoginActivity.kt
@@ -2,11 +2,49 @@ package com.egci428.feedme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    //private val mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val mAuth = FirebaseAuth.getInstance()
+
+        val email = input_email.text.toString()
+        val password = input_password.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                //Log.w(FragmentActivity., "signInWithEmail:success")
+                val user = mAuth.currentUser
+                //updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                //Log.w(FragmentActivity.TAG, "signInWithEmail:failure", task.exception)
+                Toast.makeText(this@LoginActivity, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                //updateUI(null)
+            }
+
+            // ...
+        }
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.getCurrentUser()
+        //updateUI(currentUser)
     }
 }
--- app/src/main/java/com/egci428/feedme/SignupActivity.kt
+++ app/src/main/java/com/egci428/feedme/SignupActivity.kt
@@ -2,11 +2,50 @@ package com.egci428.feedme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+//import jdk.nashorn.internal.runtime.ECMAException.getException
+import com.google.firebase.auth.FirebaseUser
+//import org.junit.experimental.results.ResultMatchers.isSuccessful
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signup.*
+
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+
+
+        val mAuth = FirebaseAuth.getInstance()
+
+        val email = input_email_signup.text.toString()
+        val password = input_password_signup.text.toString()
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        //Log.d(FragmentActivity.TAG, "createUserWithEmail:success")
+                        val user = mAuth.getCurrentUser()
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        //Log.w(FragmentActivity.TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this@SignupActivity, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+
+                    // ...
+                })
     }
 }
