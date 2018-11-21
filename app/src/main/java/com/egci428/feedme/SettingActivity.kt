031960200380933db486c84882a05c0778036538
Doikham <big_sethawit@hotmail.com>
2018-11-21 20:18:00 +0700

    Add layout to Home Page

--- app/src/main/java/com/egci428/feedme/SettingActivity.kt
+++ app/src/main/java/com/egci428/feedme/SettingActivity.kt
@@ -2,11 +2,14 @@ package com.egci428.feedme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        //editInfo.setOnClickListener(R.id.)
     }
 }
