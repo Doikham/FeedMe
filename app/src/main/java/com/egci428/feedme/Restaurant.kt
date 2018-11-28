9e84da67f3375b53da50bf5b1ab8fae12cc01b87
DESKTOP-I58HO27\Neramit Singh <neramit.singh@gmail.com>
2018-11-28 21:00:00 +0700

    Create class for writing to db (initial)

--- app/src/main/java/com/egci428/feedme/Restaurant.kt
+++ app/src/main/java/com/egci428/feedme/Restaurant.kt
@@ -8,7 +8,6 @@ import com.google.android.gms.location.places.PlacePhotoResponse
 import com.google.android.gms.maps.model.LatLng
 
 class Restaurant(val name: String,val address: String, val id: String, val phonenumber: String, val pricelevel: Int, val rating: Float, val latlng: LatLng, val mGeoDataClient: GeoDataClient) {
-    
     override fun toString(): String {
         return super.toString()
     }
